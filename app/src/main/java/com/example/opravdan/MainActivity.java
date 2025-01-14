560bf32de5c2996c19ac41cdf655a6e22537016a
Tinty <vladimirhomutov55917@gmail.com>
2025-01-14 21:29:13 +0300

    Убрал безполезный участок кода с проверкой на наличие переводов строк. Он был не нужен так как в том тексте в принципе нельзя поставить перевод на новую строку.

--- app/src/main/java/com/example/opravdan/MainActivity.java
+++ app/src/main/java/com/example/opravdan/MainActivity.java
@@ -29,9 +29,6 @@ public class MainActivity extends AppCompatActivity {
         // Сделал удаление лишних пробелов в конце и начале промпта
         // Сделал удаление переносов строк в промпте
         String user_text_prompt = returnText().strip();
-        if (user_text_prompt.contains("\n")) {
-            user_text_prompt = user_text_prompt.replaceAll("\n", ". ");
-        }
         if (!(returnText().isEmpty())){
             Intent intent = new Intent(MainActivity.this, Screen2.class);
             intent.putExtra("user_text_prompt", user_text_prompt);
